55d0abd258f9a562b78143e84e57445696f4ebdd
Insomnia <tbogucki9@gmail.com>
2023-11-22 22:07:13 +0100

    new version with https

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import select
 
 class ProxyServer:
     def __init__(self, host, port):
@@ -8,6 +9,7 @@ class ProxyServer:
 
     def start(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((self.host, self.port))
         self.socket.listen(100)
         print(f"Proxy Server listening on {self.host}:{self.port}")
@@ -19,22 +21,23 @@ class ProxyServer:
 
     def handle_client(self, client_socket):
         request = client_socket.recv(1024)
-        first_line = request.split('\n')[0]
-        url = first_line.split(' ')[1]
+        first_line = request.split(b'\n')[0]
+        method, url, protocol = first_line.split()
 
+        if method.upper() == 'CONNECT':
+            self.handle_https_connection(client_socket, url)
+        else:
+            self.handle_http_request(client_socket, request, url)
 
-        # Sprawdzenie, czy URL zawiera 'https://google.com'
-        if "https://google.pl" in url.decode('utf-8'):
-            print("GOOGLE")
-
-        http_pos = url.find("://")
+    def handle_http_request(self, client_socket, request, url):
+        http_pos = url.find(b"://")
         if (http_pos == -1):
             temp = url
         else:
-            temp = url[(http_pos+3):]
+            temp = url[(http_pos + 3):]
 
-        port_pos = temp.find(":")
-        webserver_pos = temp.find("/")
+        port_pos = temp.find(b":")
+        webserver_pos = temp.find(b"/")
         if webserver_pos == -1:
             webserver_pos = len(temp)
 
@@ -44,28 +47,59 @@ class ProxyServer:
             port = 80
             webserver = temp[:webserver_pos]
         else:
-            port = int((temp[(port_pos+1):])[:webserver_pos - port_pos - 1])
+            port = int((temp[(port_pos + 1):])[:webserver_pos - port_pos - 1])
             webserver = temp[:port_pos]
 
-        self.forward_request(webserver, port, request, client_socket)
+        self.forward_http_request(webserver, port, request, client_socket)
 
-    def forward_request(self, host, port, request, client_socket):
+    def forward_http_request(self, host, port, request, client_socket):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        server_socket.bind(('192.168.0.100', 0))
+
+        try:
+            server_socket.connect((host, port))
+            server_socket.sendall(request.encode())
+
+            while True:
+                data = server_socket.recv(4096)
+                if len(data) > 0:
+                    client_socket.send(data)
+                else:
+                    break
+        except Exception as e:
+            print(str(e))
+
+    def handle_https_connection(self, client_socket, url):
+        target_host, target_port = url.split(b':')
+
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind(('192.168.0.100', 0))
 
-        server_socket.connect((host, port))
-        server_socket.sendall(request.encode())
+        server_socket.connect((target_host, int(target_port)))
+        client_socket.sendall(b'HTTP/1.1 200 Connection established\n\n')
 
+        # Tunelowanie ruchu między klientem a serwerem
+        self.tunnel_data(client_socket, server_socket)
+
+    def tunnel_data(self, client_socket, server_socket):
+        sockets = [client_socket, server_socket]
         while True:
-            data = server_socket.recv(4096)
-            if len(data) > 0:
-                client_socket.send(data)
-            else:
+            read_sockets, write_sockets, error_sockets = select.select(sockets, [], sockets, 0)
+            if error_sockets:
                 break
 
-        server_socket.close()
+            for read_socket in read_sockets:
+                other_socket = server_socket if read_socket is client_socket else client_socket
+                data = read_socket.recv(4096)
+                if not data:
+                    break
+                other_socket.sendall(data)
+
         client_socket.close()
+        server_socket.close()
 
 if __name__ == '__main__':
-    proxy = ProxyServer('0.0.0.0', 2137)
+    proxy = ProxyServer('0.0.0.0', 2141)
     proxy.start()
